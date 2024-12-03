1a5ae51ca02eed21487fae6f338e1abf1aa6b35f
bishoysameh <bishoysamehmagdy@gmail.com>
2024-12-03 14:40:53 +0200

    i

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from typer-learn!")
+import typer
 
+app = typer.Typer()
 
+@app.command()
+def hello(name: str, age: int):
+    print(f"hello {name}, age {age}")
+@app.command()
+def goodbye():
+    print("goodbye")
 if __name__ == "__main__":
-    main()
+    app()
